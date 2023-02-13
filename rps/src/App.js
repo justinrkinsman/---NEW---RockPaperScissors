83e54e7137be7e0f655425f48c7835f3854f0d6a
Justin <justinrkinsman@gmail.com>
2023-02-13 16:42:24 -0400

    add function to check for name

--- rps/src/App.js
+++ rps/src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState("");
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const checkForName = () => {
+    if (name === "") {
+      console.log("Access Denied");
+    } else {
+      console.log("Access Granted");
+    }
+  };
+
   return (
     <div className="App">
       <h1>Rock Paper Scissors</h1>
-      <form>
+      <form className='name-form'>
         <label htmlFor="name">Enter your name </label>
-        <input id="name" type="text" />
+        <input id="name" type="text" value={name} onChange={handleNameChange}/>
       </form>
-      <button>Start</button>
+      <button onClick={checkForName}>Start</button>
     </div>
   );
 }
