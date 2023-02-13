5371b7935a3efadf7d01e066173c920a745ed2e8
Justin <justinrkinsman@gmail.com>
2023-02-13 16:11:13 -0400

    Add css styling to main page

--- rps/src/App.js
+++ rps/src/App.js
@@ -4,6 +4,11 @@ function App() {
   return (
     <div className="App">
       <h1>Rock Paper Scissors</h1>
+      <form>
+        <label htmlFor="name">Enter your name </label>
+        <input id="name" type="text" />
+      </form>
+      <button>Start</button>
     </div>
   );
 }
