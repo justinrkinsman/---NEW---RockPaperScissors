d34c9747bd68c9dc85e512aa13969ad33cc6fa0d
Justin <justinrkinsman@gmail.com>
2023-02-15 17:26:09 -0400

    Add additional animation to game screen

--- rps/src/GameScreen.js
+++ rps/src/GameScreen.js
@@ -1,21 +1,26 @@
 import React from 'react';
+import './gameScreen.css'
 
 function GameScreen(props) {
     return (
         <div>
             <p>Welcome {props.name}</p>
-            <p>Choose your weapon</p>
-            <button onClick={props.handleUserSelection}>Rock</button>
-            <button onClick={props.handleUserSelection}>Paper</button>
-            <button onClick={props.handleUserSelection}>Scissors</button>
-            <div className='scoreboard'>
-              <div className="player1">
-                <p>{props.name}</p>
-                <p>- {props.userScore}</p>
-              </div>
-              <div className='cpu-player'>
-                <p>{props.cpuScore} -</p>
-                <p>Computer</p>
+            <div className="slide-in-one">
+              <img src="https://see.fontimg.com/api/renderfont4/aoWm/eyJyIjoiZnMiLCJoIjo1MywidyI6MTAwMCwiZnMiOjUzLCJmZ2MiOiIjMDAwMDAwIiwiYmdjIjoiIzgyOEMzMCIsInQiOjF9/Q2hvb3NlIFlvdXIgV2VhcG9u/boss-fight.png" alt="Boss fonts"></img>
+            </div>
+            <div class="game-body">
+              <button onClick={props.handleUserSelection}>Rock</button>
+              <button onClick={props.handleUserSelection}>Paper</button>
+              <button onClick={props.handleUserSelection}>Scissors</button>
+              <div className='scoreboard'>
+                <div className="player1">
+                  <p>{props.name}</p>
+                  <p>- {props.userScore}</p>
+                </div>
+                <div className='cpu-player'>
+                  <p>{props.cpuScore} -</p>
+                  <p>Computer</p>
+                </div>
               </div>
             </div>
             {props.roundWinner && <p>{props.roundWinner}</p>}
